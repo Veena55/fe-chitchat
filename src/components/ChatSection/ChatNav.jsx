7f8886314c6c7b7f5844916be60368c1858d3c64
VEENA SURYAPRAKASH RAO <53285960+Veena55@users.noreply.github.com>
2024-07-03 20:50:12 +0530

    Chat (#9)
    * inital setup
    
    * inital_setup
    
    * react-setup-files
    
    * Login setup
    
    * Login UI
    
    * regsiter-form-UI
    
    * regsiter-form-UI
    
    * updated inputs
    
    * fixing some animation, setting theme
    
    * mail-setup
    
    * mail send & veriy -otp
    
    * chat-navbar-setup
    
    * ChatArea page
    
    * chat-area-setup
    
    * invite-page
    
    ---------

--- src/components/ChatSection/ChatNav.jsx
+++ src/components/ChatSection/ChatNav.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { BsBellFill, BsChatDots } from 'react-icons/bs'
+import { BsChatDots } from 'react-icons/bs'
 import { LuContact } from 'react-icons/lu'
 import './chat.css';
+import { FaUserFriends, FaUserPlus } from 'react-icons/fa';
 
 const ChatNav = () => {
     const [activeElement, setActiveElement] = useState(['chats']);
@@ -10,20 +11,18 @@ const ChatNav = () => {
     }
     return (
         <div className='row mx-0'>
-            <div className="col-12 py-2 d-flex justify-content-between chat-nav">
+            <div className="col-12 py-2 d-flex justify-content-between align-items-center chat-nav">
                 <div>
-                    <h4 className='text-primary fs-bold text-capitalize'>{activeElement}</h4>
+                    <h5 className='text-primary fs-bold text-capitalize'>{activeElement}</h5>
                 </div>
                 <div className='d-flex align-items-center'>
                     <BsChatDots className={`text-secondary mx-1 ${activeElement == 'chats' && 'active'}`} onClick={() => setActiveClass('chats')} size={20} />
                     <LuContact className={`text-secondary mx-1 ${activeElement == 'contacts' && 'active'}`} onClick={() => setActiveClass('contacts')} size={20} />
-                    <div className='position-relative'>
-                        <BsBellFill className={`text-secondary mx-1 ${activeElement == 'notifications' && 'active'}`} onClick={() => setActiveClass('notifications')} size={20} />
-                        <div className='position-absolute notification_count translate-middle badge rounded-circle bg bg-danger'><small>12</small></div>
-                    </div>
+                    <FaUserFriends className={`text-secondary mx-1 ${activeElement == 'groups' && 'active'}`} onClick={() => setActiveClass('groups')} size={20} />
+                    <FaUserPlus className={`text-danger mx-1 ${activeElement == 'invite' && 'active'}`} onClick={() => setActiveClass('invite')} size={20} />
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 export default ChatNav
---
+++ src/components/Invite/Invite.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Invite.css'
+
+const Invite = () => {
+    return (
+        <div className='row mx-0 bg-light-blue align-items-center' style={{ height: "100vh" }}>
+            <div className="col-6 mx-auto">
+                <h5 className='text-primary'>Send Invite link to your friend</h5>
+                <div className="form-group">
+                    <input type="email" className="form-control" readOnly />
+                </div>
+                <div className='mt-2 text-end'>
+                    <button className="btn btn-primary">Send Invite</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Invite
