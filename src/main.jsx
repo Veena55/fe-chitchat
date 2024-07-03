616b768ff904fd5c3fe3970dbe212c24abc23e0c
VEENA SURYAPRAKASH RAO <veenasrao5@gmail.com>
2024-07-03 20:40:05 +0530

    invite-page

--- src/main.jsx
+++ src/main.jsx
@@ -9,19 +9,25 @@ import Verify from './components/Verify/Verify.jsx';
 import { ToastContainer } from 'react-toastify';
 import ToggleButton from './components/ToggleButton/ToggleButton.jsx';
 import Home from './pages/Home/Home.jsx';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Invite from './components/Invite/Invite.jsx';
+// Create a new QueryClient instance
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <ToastContainer />
-    <ToggleButton />
-    <Routes>
-      <Route path='/' element={<App />}></Route>
-      <Route path='/home' element={<Home />}></Route>
-      <Route path='/login' element={<Login />}></Route>
-      <Route path='/verify' element={<Verify />}></Route>
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <ToastContainer />
+      <ToggleButton />
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/verify' element={<Verify />}></Route>
+        <Route path='/invite' element={<Invite />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
   // </React.StrictMode>,
 )
